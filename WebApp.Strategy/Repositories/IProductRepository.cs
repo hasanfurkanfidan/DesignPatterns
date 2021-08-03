6b5556c180364a752606cb5a3f6dbff04a4923ec
hasanfurkanfidan <furkanfidan.job@gmail.com>
2021-08-03 23:32:21 +0300

    ilerleme sağlandı

---
+++ WebApp.Strategy/Repositories/IProductRepository.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WebApp.Strategy.Entities;
+
+namespace WebApp.Strategy.Repositories
+{
+    public interface IProductRepository
+    {
+        Task<Product> GetByIdAsync(string id);
+        Task<List<Product>> GetAllByUserIdAsync(string userId);
+        Task AddAsync(Product product);
+        Task UpdateAsync(Product product);
+        Task DeleteAsync(Product product);
+    }
+}
---
+++ WebApp.Strategy/Repositories/ProductRepositoryFromSqlServer.cs
@@ -0,0 +1,46 @@
+﻿using BaseProject.DataAccess;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WebApp.Strategy.Entities;
+
+namespace WebApp.Strategy.Repositories
+{
+    public class ProductRepositoryFromSqlServer : IProductRepository
+    {
+        private readonly Context _context;
+        public ProductRepositoryFromSqlServer(Context context)
+        {
+            _context = context;
+        }
+        public async Task AddAsync(Product product)
+        {
+            await _context.AddAsync(product);
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task DeleteAsync(Product product)
+        {
+            _context.Remove(product);
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task<List<Product>> GetAllByUserIdAsync(string userId)
+        {
+            return await _context.Set<Product>().Where(p => p.AppUserId == userId).ToListAsync();
+        }
+
+        public async Task<Product> GetByIdAsync(string id)
+        {
+            return await _context.Set<Product>().Where(p => p.Id == id).FirstOrDefaultAsync();
+        }
+
+        public async Task UpdateAsync(Product product)
+        {
+            _context.Update(product);
+            await _context.SaveChangesAsync();
+        }
+    }
+}
