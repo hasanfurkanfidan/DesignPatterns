f6d42230fa3af3f7037a907bd46a236eaafa32b5
hasanfurkanfidan <furkanfidan.job@gmail.com>
2021-08-10 12:45:29 +0300

    İlerleme sağlandı

--- WebApp.Command/Controllers/HomeController.cs
+++ WebApp.Command/Controllers/HomeController.cs
@@ -1,5 +1,7 @@
-﻿using BaseProject.Models;
+﻿using BaseProject.DataAccess;
+using BaseProject.Models;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Logging;
 using System;
 using System.Collections.Generic;
@@ -12,17 +14,22 @@ namespace BaseProject.Controllers
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
-
-        public HomeController(ILogger<HomeController> logger)
+        private readonly Context _context;
+        public HomeController(ILogger<HomeController> logger,Context context)
         {
+            _context = context;
             _logger = logger;
         }
 
-        public IActionResult Index()
+        public async Task< IActionResult >Index()
         {
             return View();
         }
+        public async Task<IActionResult> Products()
+        {
+            return View(await _context.Products.ToListAsync());
 
+        }
         public IActionResult Privacy()
         {
             return View();
