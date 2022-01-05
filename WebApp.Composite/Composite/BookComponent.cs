1c355c045348d74414890319d61f871452220e39
Hasan Furkan Fidan <62479737+hasanfurkanfidan@users.noreply.github.com>
2022-01-05 11:12:10 +0300

    ilerleme sağlandı

---
+++ WebApp.Composite/Composite/BookComponent.cs
@@ -0,0 +1,22 @@
+﻿namespace WebApp.Composite.Composite
+{
+    public class BookComponent : IBookComponent
+    {
+        public BookComponent(int id, string name)
+        {
+            Id = id;
+            Name = name;
+        }
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int Count()
+        {
+            return 1;
+        }
+
+        public string Display()
+        {
+            return $"<li class='list-group-item'>{Name}</li>";
+        }
+    }
+}
---
+++ WebApp.Composite/Composite/IBookComponent.cs
@@ -0,0 +1,10 @@
+﻿namespace WebApp.Composite.Composite
+{
+    public interface IBookComponent
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        int Count();
+        string Display();
+    }
+}
